99296a1bdfd8f59a2c5bcaf38fea94cd4d2238eb
Leo <leonardo-f98@hotmail.com>
2023-03-19 16:11:20 -0300

    Segunda lista finalizada

---
+++ exercicio-Lucas/segunda-lista/exercicio1-04.03/main.js
@@ -0,0 +1,24 @@
+/*1. Elaborar um programa que leia 12 elementos numéricos  
+inteiros em uma matriz do tipo vetor. Coloque-os em ordem  
+decrescente e apresente os elementos ordenados*/
+
+let matriz = [
+  [0, 1, 2],
+  [52, 35, 3],
+  [12, 59, 75],
+  [5, 29, 71],
+];
+
+let valores = [];
+for (let i = 0; i <= 3; i++) {
+  for (let j = 0; j <= 2; j++) {
+    valores.push(matriz[i][j]);
+  }
+}
+
+function sortfunction(a, b) {
+  return b - a;
+}
+valores.sort(sortfunction);
+
+console.log(valores);
